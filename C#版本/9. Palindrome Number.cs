76286afa2c87cfdba05992158a3b7109cb41708b
ZhangChao <zhangchaoa@buaa.edu.cn>
2019-08-02 10:34:54 +0800

    Update 9. Palindrome Number.cs

--- C#版本/9. Palindrome Number.cs
+++ C#版本/9. Palindrome Number.cs
@@ -1,1 +1,19 @@
-
+//回文数
+//负数一定不是回文数，正数则先计算出倒序数值，再与原数值进行比较
+public class Solution {
+    public bool IsPalindrome(int x) {
+        int num = x ;
+        int cur = 0 ;
+        
+        if ( x < 0 )
+        {
+            return false ;
+        }
+        while ( num != 0 )
+        {
+            cur = cur * 10 + num % 10 ;
+            num /= 10 ;
+        }
+        return cur == x ;
+    }
+}
