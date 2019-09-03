2c79bee9d4874f23d8bf517676a41a5bc957ff01
ZhangChao <zhangchaoa@buaa.edu.cn>
2019-09-03 14:29:50 +0800

    Update A10.矩形覆盖.py

--- 剑指Offer-Python版本/A10.矩形覆盖.py
+++ 剑指Offer-Python版本/A10.矩形覆盖.py
@@ -3,7 +3,21 @@
 '''
 
 '''
-方法：递归问题，依然是斐波那契数列的变形。代码参考跳台阶。
-运行时间：24ms
-占用内存：5752k
+方法：递归问题，实际上就是普通的跳台阶问题，一般这类问题就是从后往前思考。
+F（0）= 0
+F（1）= 1
+F（2）= 2
+F（n）= F（n-1）+ F（n-2）（n > 2）
+运行时间：29ms
+占用内存：5912k
 '''
+
+class Solution:
+    def rectCover(self, number):
+        # write code here
+        if number<=2:
+            return number
+        prev,curr=1,2
+        for i in range(3,number+1):
+            prev,curr=curr,prev+curr
+        return curr
