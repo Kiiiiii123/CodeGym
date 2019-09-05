27cd6be14c2f1131e2336980f50093b9f5f48d35
ZhangChao <zhangchaoa@buaa.edu.cn>
2019-09-05 14:38:54 +0800

    Update A12.数值的整数次方.py

--- 剑指Offer-Python版本/A12.数值的整数次方.py
+++ 剑指Offer-Python版本/A12.数值的整数次方.py
@@ -4,6 +4,51 @@
 
 '''
 方法一：使用循环。exponent是整数，可能是0、正数或者负数。当其为正数时直接使用循环；当其为负数先求出其绝对值的结果，再取倒数；当其为0时直接返回base。
-运行时间：
-占用内存：
+运行时间：28ms
+占用内存：5872k
 '''
+
+class Solution:
+    def Power(self, base, exponent):
+        # write code here
+        if exponent<0:
+            return 1/self.getPower(base,-exponent)
+        elif exponent==0:
+            return 1
+        else:
+            return self.getPower(base,exponent)
+        
+    def getPower(self,base,exponent):
+        if base==0:
+            return 0
+        result=1
+        for i in range(exponent):
+            result*=base
+        return result
+
+'''
+方法二：exponent分为偶数和奇数，通过递归提高运行速度
+运行时间：27ms
+占用内存：5752k
+'''
+
+class Solution:
+    def Power(self, base, exponent):
+        # write code here
+        if exponent<0:
+            return 1/self.getPower(base,-exponent)
+        elif exponent==0:
+            return 1
+        elif exponent==1:
+            return base
+        else:
+            return self.getPower(base,exponent)
+        
+    def getPower(self,base,exponent):
+        if base==0:
+            return 0
+        result=self.Power(base,exponent>>1)
+        result*=result
+        if exponent&1==1:
+            result*=base
+        return result
