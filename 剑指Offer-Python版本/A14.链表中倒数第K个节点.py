ceb55cba72187cfad228d3d1678cce574fbf8dac
ZhangChao <zhangchaoa@buaa.edu.cn>
2019-09-07 19:48:10 +0800

    Update A14.链表中倒数第K个节点.py

--- 剑指Offer-Python版本/A14.链表中倒数第K个节点.py
+++ 剑指Offer-Python版本/A14.链表中倒数第K个节点.py
@@ -31,9 +31,30 @@ class Solution:
 
 '''
 方法二：设置两个指针指向头节点，第一个指针向前走k-1步，走到第k个结点，此时，第二个指针和第一个指针同时移动，当第一个指针到尾节点的时候，第二个指针指向倒数第k个结点。
-运行时间：23ms
-占用内存：5728k
+运行时间：33ms
+占用内存：5624k
 '''
+
+#class ListNode:
+#     def __init__(self, x):
+#            self.val = x  
+#            self.next = None
+
+class Solution:
+    def FindKthToTail(self, head, k):
+        # write code here
+        if head==None or k<=0:
+            return None
+        p1=head
+        p2=head
+        for i in range(k-1):
+            if p1.next==None:
+                return None
+            p1=p1.next
+        while p1.next:
+            p1=p1.next
+            p2=p2.next
+        return p2
 
 
 
