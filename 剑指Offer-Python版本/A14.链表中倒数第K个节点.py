0bd790029d5d815420b4066085b418eb12162ecb
ZhangChao <zhangchaoa@buaa.edu.cn>
2019-09-07 17:36:33 +0800

    Update A14.链表中倒数第K个节点.py

--- 剑指Offer-Python版本/A14.链表中倒数第K个节点.py
+++ 剑指Offer-Python版本/A14.链表中倒数第K个节点.py
@@ -4,10 +4,30 @@
 
 '''
 方法一：遍历一次链表获得链表长度，再次遍历链表，至n-k+1出输出
-运行时间：27ms
-占用内存：5724k
+运行时间：23ms
+占用内存：5728k
 '''
 
+#class ListNode:
+#     def __init__(self, x):
+#            self.val = x  
+#            self.next = None
+
+class Solution:
+    def FindKthToTail(self, head, k):
+        # write code here
+        if head==None or k<=0:
+            return None
+        p=head
+        n=1
+        while p.next!=None:
+            p=p.next
+            n=n+1
+        if k>n:
+            return None
+        for i in range(n-k):
+            head=head.next
+        return head
 
 
 
