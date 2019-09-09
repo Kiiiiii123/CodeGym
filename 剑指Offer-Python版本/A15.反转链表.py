fddf6c6e66f6251a4f875cc0b734bbdaac9561dd
ZhangChao <zhangchaoa@buaa.edu.cn>
2019-09-09 14:19:54 +0800

    Update A15.反转链表.py

--- 剑指Offer-Python版本/A15.反转链表.py
+++ 剑指Offer-Python版本/A15.反转链表.py
@@ -3,7 +3,25 @@
 '''
 
 '''
-方法：
-运行时间：23ms
-占用内存：5728k
+方法：迭代法
+运行时间：26ms
+占用内存：5844k
 '''
+
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+class Solution:
+    # 返回ListNode
+    def ReverseList(self, pHead):
+        # write code here
+        if not pHead or not pHead.next:
+            return pHead
+        last=None
+        while pHead:
+            temp=pHead.next
+            pHead.next=last
+            last=pHead
+            pHead=temp
+        return last
