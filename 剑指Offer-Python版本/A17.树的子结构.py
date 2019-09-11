24042bb611d270409b58dc4ae845de469273c6fa
ZhangChao <zhangchaoa@buaa.edu.cn>
2019-09-11 15:11:02 +0800

    Update A17.树的子结构.py

--- 剑指Offer-Python版本/A17.树的子结构.py
+++ 剑指Offer-Python版本/A17.树的子结构.py
@@ -7,3 +7,24 @@
 运行时间：32ms
 占用内存：5752k
 '''
+
+class Solution:
+    def HasSubtree(self, pRoot1, pRoot2):
+        # write code here
+        if pRoot1==None or pRoot2==None:
+            return False
+        result=False
+        if pRoot1.val==pRoot2.val:
+            result=self.isSubtree(pRoot1,pRoot2)
+        if result==False:
+            result=self.HasSubtree(pRoot1.left,pRoot2) or self.HasSubtree(pRoot1.right,pRoot2)
+        return result
+        
+    def isSubtree(self,root1,root2):
+        if root2==None:
+            return True
+        if root1==None:
+            return False
+        if root1.val==root2.val:
+            return self.isSubtree(root1.left,root2.left) and self.isSubtree(root1.right,root2.right)
+        return False
