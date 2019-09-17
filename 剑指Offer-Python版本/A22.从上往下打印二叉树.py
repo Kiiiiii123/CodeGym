0a8a102a3130adb356bba9c16708c6703344c9b6
ZhangChao <zhangchaoa@buaa.edu.cn>
2019-09-17 14:05:38 +0800

    Update A22.从上往下打印二叉树.py

--- 剑指Offer-Python版本/A22.从上往下打印二叉树.py
+++ 剑指Offer-Python版本/A22.从上往下打印二叉树.py
@@ -4,6 +4,29 @@
 
 '''
 方法：使用辅助列表
-运行时间：28ms
-占用内存：5856k
+运行时间：24ms
+占用内存：5732k
 '''
+
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+class Solution:
+    # 返回从上到下每个节点值列表，例：[1,2,3]
+    def PrintFromTopToBottom(self, root):
+        # write code here
+        queue=[]
+        result=[]
+        if not root:
+            return result
+        queue.append(root)
+        while queue:
+            Node=queue.pop(0)
+            result.append(Node.val)
+            if Node.left:
+                queue.append(Node.left)
+            if Node.right:
+                queue.append(Node.right)
+        return result
