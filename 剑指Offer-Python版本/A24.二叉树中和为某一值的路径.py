a2141b49ce89bf5f251966226aab5aca245608cb
ZhangChao <zhangchaoa@buaa.edu.cn>
2019-09-24 22:59:54 +0800

    Update A24.二叉树中和为某一值的路径.py

--- 剑指Offer-Python版本/A24.二叉树中和为某一值的路径.py
+++ 剑指Offer-Python版本/A24.二叉树中和为某一值的路径.py
@@ -7,3 +7,24 @@
 运行时间：24ms
 占用内存：5728k
 '''
+
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+class Solution:
+    # 返回二维列表，内部每个列表表示找到的路径
+    def FindPath(self, root, expectNumber):
+        # write code here
+        result=[]
+        if not root or root.val>expectNumber:
+            return []
+        if not root.left and not root.right and root.val==expectNumber:
+            return [[root.val]]
+        else:
+            left=self.FindPath(root.left,expectNumber-root.val)
+            right=self.FindPath(root.right,expectNumber-root.val)
+            for item in left+right:
+                result.append([root.val]+item)
+            return sorted(result,key=lambda x:-len(x))
