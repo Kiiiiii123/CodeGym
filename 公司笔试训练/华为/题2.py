f64b752b03777bcc1d3b559bab5fa246202694f9
ZhangChao <zhangchaoa@buaa.edu.cn>
2019-09-26 13:12:13 +0800

    Update 题2.py

--- 公司笔试训练/华为/题2.py
+++ 公司笔试训练/华为/题2.py
@@ -5,4 +5,25 @@
 
 '''
 方法： 质数（Prime number，又称素数），指在大于1的自然数中，除了1和该数自身外，无法被其他自然数整除的数（也可定义为只有1与该数本身两个正因数的数）
+这里分两步处理问题。
 '''
+
+if __name__=='__main__':
+  number=input()
+  low=int(number.split(' ')[0])
+  high=int(number.split(' ')[1])
+  
+  m=n=0
+  list=[]
+  for i in range(low,high):
+    for j in range(2,i):
+      if i%j==0:
+        break;
+    list.append(i)
+  for item in list:
+    m+=item%10
+    n+=item//10%10
+  if m>n:
+    print(n)
+  else:
+    print(m)
