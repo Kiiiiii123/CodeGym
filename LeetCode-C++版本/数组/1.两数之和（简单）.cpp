560972425aaa26cbb95943042f656ab2c7332aa7
ZhangChao <zhangchaoa@buaa.edu.cn>
2019-10-10 23:04:59 +0800

    Update 1.两数之和（简单）.cpp

--- LeetCode-C++版本/数组/1.两数之和（简单）.cpp
+++ LeetCode-C++版本/数组/1.两数之和（简单）.cpp
@@ -36,3 +36,31 @@ public:
 空间复杂度：O(n) 所需的额外空间取决于哈希表中存储的元素数量，该表中存储了n个元素。
 */
 
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        map<int,int> a;  /*map是STL的一个关联容器，它提供一对一（其中第一个可以称为关键字，每个关键字只能在map中出现一次，第二个可能称为该关键字的值）的数据处理能力*/
+        vector<int> b(2,-1);  
+        for(int i=0;i<nums.size();i++)
+            a.insert(map<int,int>::value_type(nums[i],i));
+        for(int i=0;i<nums.size();i++)
+        {
+            if(a.count(target-nums[i])>0 && a[target-nums[i]]!=i)
+            {
+                b[0] = i;
+                b[1] = a[target-nums[i]];
+                break;
+            }
+        }
+        return b ;
+    }
+};
+
+/*方法三：一遍哈希表
+在进行迭代并将元素插入到表中的同时，我们还会回过头来检查表中是否已经存在当前元素所对应的目标元素。如果它存在，那我们已经找到了对应解，并立即将其返回。
+时间复杂度：O（n）我们只遍历了包含有n个元素的列表一次。在表中进行的每次查找只花费O(1)的时间。
+空间复杂度：O（n）所需的额外空间取决于哈希表中存储的元素数量，该表最多需要存储n个元素。
+*/
+
+
+
