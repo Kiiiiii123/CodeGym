702ba39e36dc17161e0346bd4f72f00a99204e4f
ZhangChao <zhangchaoa@buaa.edu.cn>
2019-10-15 23:50:07 +0800

    Update 144.二叉树的前序遍历（中等）.cpp

--- LeetCode-C++版本/树/144.二叉树的前序遍历（中等）.cpp
+++ LeetCode-C++版本/树/144.二叉树的前序遍历（中等）.cpp
@@ -15,3 +15,29 @@
 时间复杂度：Ｏ(n)　递归函数T(n) = 2T(n/2)+1。
 空间复杂度：最坏情况下需要Ｏ(n)，平均情况下需要Ｏ(lgn)。
 */
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+private:
+    vector<int> res;
+public:
+    vector<int> preorderTraversal(TreeNode* root) {
+        if(!root)
+            return res;
+        else
+        {
+            res.push_back(root->val);
+            preorderTraversal(root->left);
+            preorderTraversal(root->right);  
+        }
+        return res; 
+    }
+};
