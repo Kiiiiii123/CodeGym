7eee3bad05738938314f3b2d212a00211c0ac3a1
ZhangChao <zhangchaoa@buaa.edu.cn>
2019-10-16 15:47:31 +0800

    Update 144.二叉树的前序遍历（中等）.cpp

--- LeetCode-C++版本/树/144.二叉树的前序遍历（中等）.cpp
+++ LeetCode-C++版本/树/144.二叉树的前序遍历（中等）.cpp
@@ -41,3 +41,39 @@ public:
         return res; 
     }
 };
+
+/*方法二：迭代法
+弹出栈顶元素并输出后先压入右节点再压入左节点。
+*/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> preorderTraversal(TreeNode* root) {
+        stack<TreeNode*> stack;
+        vector<int> res;
+        if(!root)
+            return res;
+        stack.push(root);
+        while(!stack.empty())
+        {
+            TreeNode* tmp = stack.top();
+            stack.pop();
+            if(tmp)
+            {
+                res.push_back(tmp->val);
+                stack.push(tmp->right);  //先压入右子树而不是左子树是根据栈后入先出的特点
+                stack.push(tmp->left);
+            }
+        }
+        return res;
+    }
+};
