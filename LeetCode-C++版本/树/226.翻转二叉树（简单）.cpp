bbf7f6d44eadb41ac29fae2b43f8e5dc87f5c9dc
ZhangChao <zhangchaoa@buaa.edu.cn>
2019-10-17 10:47:29 +0800

    Update 226.翻转二叉树（简单）.cpp

--- LeetCode-C++版本/树/226.翻转二叉树（简单）.cpp
+++ LeetCode-C++版本/树/226.翻转二叉树（简单）.cpp
@@ -24,5 +24,30 @@
 */
 
 /*方法三：递归
-
+１、判断节点是否为空，为空则返回NULL;
+２、交换根节点的左右节点；
+３、递归交换左右子树。
 */
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* invertTree(TreeNode* root) {
+        if(!root)
+            return NULL;
+        TreeNode* tmp = root->left;
+        root->left = root->right;
+        root->right = tmp;
+        invertTree(root->left);
+        invertTree(root->right);
+        return root;
+    }
+};
