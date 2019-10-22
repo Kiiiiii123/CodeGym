6b99454e0d65a0989f1cee3241491b146cf8b811
ZhangChao <zhangchaoa@buaa.edu.cn>
2019-10-22 10:10:07 +0800

    Update 107.二叉树的层次遍历 II（简单）.cpp

--- LeetCode-C++版本/树/107.二叉树的层次遍历 II（简单）.cpp
+++ LeetCode-C++版本/树/107.二叉树的层次遍历 II（简单）.cpp
@@ -12,3 +12,45 @@
   [3]
 ]
 */
+
+/*方法：使用STL
+利用二叉树的层次遍历的解法，将最后的res进行reverse(res.begin(),res.end())，快速解决。
+*/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    vector<vector<int>> levelOrderBottom(TreeNode* root) {
+        vector<vector<int>> res;
+        if(!root)
+            return res;
+        queue<TreeNode*> Q;
+        TreeNode* tmp;
+        Q.push(root);
+        while(!Q.empty())
+        {
+            vector<int> a;
+            int len = Q.size();
+            for(int i=0;i<len;++i){
+                tmp = Q.front();
+                a.push_back(tmp->val);
+                Q.pop();
+                if(tmp->left)
+                    Q.push(tmp->left);
+                if(tmp->right)
+                    Q.push(tmp->right);
+            }
+            res.push_back(a); 
+        }
+        reverse(res.begin(),res.end());
+        return res;
+    }
+};
