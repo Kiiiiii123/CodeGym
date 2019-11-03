c0f2a442fbfa50351ca7baa8b817a116b825a113
ZhangChao <zhangchaoa@buaa.edu.cn>
2019-11-04 00:48:22 +0800

    Update 572. 另一个树的子树（简单）.cpp

--- LeetCode-C++版本/树/572. 另一个树的子树（简单）.cpp
+++ LeetCode-C++版本/树/572. 另一个树的子树（简单）.cpp
@@ -18,3 +18,27 @@
 方法：递归法
 先判断两棵树是否相等，再判断s的左子树是否与t相等，再判断s的右子树是否与t相等。借用到判断是否相等的函数。
 */
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool isSubtree(TreeNode* s,TreeNode* t)
+    {
+        if(!s&&!t) return true;
+        if(!s||!t) return false;
+        return isSametree(s,t)||isSubtree(s->left,t)||isSubtree(s->right,t);
+    }
+    bool isSametree(TreeNode* s, TreeNode* t) {
+        if(!s&&!t) return true;
+        if(!s||!t) return false;
+        return s->val==t->val&&isSametree(s->left,t->left)&&isSametree(s->right,t->right);
+    }
+};
